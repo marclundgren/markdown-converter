27952840e3a4eaa3c4721c1382b4facb1b0ab096
Marc Lundgren <marclundgren2.0@gmail.com>
2016-02-06 23:25:33 -0800

    markedown converter wip

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,9 +1,18 @@
-import React, { PropTypes } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router'
-import { actions as counterActions } from '../../redux/modules/counter'
-import DuckImage from './Duck.jpg'
-import classes from './HomeView.scss'
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import { actions as counterActions } from '../../redux/modules/counter';
+import DuckImage from './Duck.jpg';
+import classes from './HomeView.scss';
+import marked from 'marked';
+
+function createMarkup(__html) {
+  return {__html};
+}
+
+function renderMarkdown(rawInput) {
+  return createMarkup(marked(rawInput));
+}
 
 // We define mapStateToProps where we'd normally use
 // the @connect decorator so the data requirements are clear upfront, but then
@@ -11,45 +20,47 @@ import classes from './HomeView.scss'
 // the component can be tested w/ and w/o being connected.
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
 const mapStateToProps = (state) => ({
-  counter: state.counter
-})
+  raw: state.raw
+});
 export class HomeView extends React.Component {
-  static propTypes = {
-    counter: PropTypes.number.isRequired,
-    doubleAsync: PropTypes.func.isRequired,
-    increment: PropTypes.func.isRequired
-  };
+  // static propTypes = {
+  //   raw: PropTypes.string.isRequired
+  // };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      raw: ''
+    };
+  }
 
   render () {
+    let raw = this.state.raw || '';
+
     return (
-      <div className='container text-center'>
+      <div className='container'>
         <div className='row'>
-          <div className='col-xs-2 col-xs-offset-5'>
-            <img className={classes.duck}
-                 src={DuckImage}
-                 alt='This is a duck, because Redux.' />
+          <h1>Markdown Converter</h1>
+          <div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+            <h3>raw markdown</h3>
+            <textarea
+              onChange={(event) => {this.setState({raw: event.target.value});}}
+              value={this.props.raw}
+              cols="30"
+              rows="10" />
+          </div>
+          <div className='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+            <h3>markdown output</h3>
+
+            <div dangerouslySetInnerHTML={renderMarkdown(raw)} />
           </div>
         </div>
-        <h1>Welcome to the React Redux Starter Kit</h1>
-        <h2>
-          Sample Counter:
-          {' '}
-          <span className={classes['counter--green']}>{this.props.counter}</span>
-        </h2>
-        <button className='btn btn-default'
-                onClick={() => this.props.increment(1)}>
-          Increment
-        </button>
-        {' '}
-        <button className='btn btn-default'
-                onClick={this.props.doubleAsync}>
-          Double (Async)
-        </button>
-        <hr />
-        <Link to='/404'>Go to 404 Page</Link>
       </div>
-    )
+    );
   }
 }
 
-export default connect(mapStateToProps, counterActions)(HomeView)
+export default HomeView;
+
+// export default connect(mapStateToProps, counterActions)(HomeView);
